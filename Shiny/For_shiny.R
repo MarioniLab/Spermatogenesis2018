c5622757216fc9ecf51d1760ed2a6977f694a7ad
Nils Eling <nils.eling@gmx.de>
2018-05-08 07:53:59 +0100

    Changed colours for shiny

--- Shiny/For_shiny.R
+++ Shiny/For_shiny.R
@@ -22,9 +22,9 @@ mouse.genes$Chromosome.scaffold.name <- paste("Chr",
                                               sep = "")
 
 # Create colour vector
-qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
+#qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
 # col_vector = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
-color_vector = c("#242435", "#D9D9ED","#D62261","#BCBCC2","#B19B31","#282B69","#572972","#421449","#4A1028","#851A4F","#643F18","#0F1131","#FFDFB2","#60AC78","#985D25","#176D38","#C58C58","#827BAF","#F36E21","#71719A","#47476B","#AFB3DA","#9595C9","#93381F","#A58D81","#8A8A9B","#2A348B","#6167AF","#5F4E97","#EE6493","#CECAE5","#ff00ff")
+color_vector = c("#47476B", "#F36E21","#985D25","#643F18","#5F4E97","#C58C58","#B19B31","#572972","#D62261","#851A4F","#FFDFB2","#71719A","#9595C9","#AFB3DA","#176D38","#421449","#EE6493","#2A348B","#827BAF","#93381F","#D9D9ED","#242435","#CECAE5","#067277","#8A8A9B","#6167AF","#0F1131","#282B69","#BCBCC2","#4A1028","#A58D81","#ff00ff")
 names(color_vector) <- levels(as.factor(colData(sce)$Clusters))
 
 # Save output
