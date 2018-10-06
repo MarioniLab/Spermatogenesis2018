2655ce4d5a1a61030be563ca52e2d1d62501a4db
Nils Eling <nils.eling@gmx.de>
2018-10-06 15:17:23 +0200

    Revert back to using the mnnCorrect function

--- Functions/auxiliary.R
+++ Functions/auxiliary.R
@@ -185,11 +185,11 @@ batch.correction <- function(sce, number.HVG = 1000){
   genes <- rownames(HVG.df)[1:number.HVG]
   
   # Batch correction
-  func <- paste0("fastMNN(", 
-                     paste0("sce[[", 1:length(sce), "]][genes,]", collapse=", "), 
-                     ")")
+  func <- paste0("mnnCorrect(", 
+                     paste0("as.matrix(logcounts(sce[[", 1:length(sce), "]])[genes,])", collapse=", "), 
+                     ", cos.norm.in=TRUE, cos.norm.out=TRUE, sigma=0.1)")
   corrected <- eval( parse(text=func) )
-  corrected$corrected
+  do.call("cbind", corrected$corrected)
 }
 
 #### DE between ambient profiles
