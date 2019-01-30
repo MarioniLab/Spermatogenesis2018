44465ed71f1711e3ffdef98c9ae47fd4b35b5fd2
nilseling <nils.eling@gmx.de>
2019-01-30 17:05:09 +0000

    Added P5 sample to shiny

--- Shiny/ui.R
+++ Shiny/ui.R
@@ -11,21 +11,24 @@ shinyUI(fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-      selectizeInput("gene", label = "Select gene", choices = NULL, options = 
-                       list(placeholder = 'Select a gene name', maxItems = 1, 
+      selectizeInput("gene", label = "Select gene", choices = NULL, options =
+                       list(placeholder = 'Select a gene name', maxItems = 1,
                             maxOptions = 10)),
       helpText("Please select the gene symbol you would like to visualize.",
                "Visualization of the expression of whole chromosomes is achieved when",
                "entering the chromosome name (e.g. ChrX)."),
-      selectizeInput("dataset", label = "Select dataset", 
-                  choices = c("B6", "P5", "P10", "P15", "P20", "P25", "P30", "P35"),
-                  options = list(placeholder = 'Select one or multiple datasets',
-                                 maxItems = 9)),
+      selectizeInput("dataset", label = "Select dataset",
+                     choices = c("B6", "P5", "P10", "P15", "P20", "P25", "P30", "P35"),
+                     options = list(placeholder = 'Select one or multiple datasets',
+                                    maxItems = 10)),
       helpText("Please select one or multiple datasets for visualization.",
                "The cells were mapped to adult B6 samples as reference."),
-      selectInput("group", label = "Select groups", 
-                     choices = NULL, multiple = TRUE),
-      helpText("Please select the somatic or germ cell types for visualization"),
+      selectInput("group", label = "Select groups",
+                  choices = NULL, multiple = TRUE),
+      helpText("Please select the somatic or germ cell types for visualization.\n",
+               "eP: early Pachytene spermatocytes, mP: mid Pachytene spermatocytes,",
+               "lP: late Pachytene spermatocytes, D: Diplotene spermatocytes, M: Meiosis,",
+               "S: Spermatid, PTM: Peritubular Myoid Cells, tMg: testicular Macrophages"),
       actionButton("goButton", "Go!")
     ),
     
@@ -33,7 +36,7 @@ shinyUI(fluidPage(
       tabPanel("Plot",
                fluidRow(
                  splitLayout(cellWidths = c("50%", "50%"), plotOutput("tsne"), plotOutput("sample"))
-               ), 
+               ),
                fluidRow(
                  splitLayout(cellWidths = c("50%", "50%"), plotOutput("cluster"), plotOutput("boxplot"))
                ))
